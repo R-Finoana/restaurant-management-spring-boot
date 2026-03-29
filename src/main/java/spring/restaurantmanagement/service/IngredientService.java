daf57855cbe672fe58dad86287c9df2be82653b8
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-03-29 17:05:04 +0300

    feat: create IngredientService

---
+++ src/main/java/spring/restaurantmanagement/service/IngredientService.java
@@ -0,0 +1,20 @@
+package spring.restaurantmanagement.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import spring.restaurantmanagement.entity.Ingredient;
+import spring.restaurantmanagement.repository.IngredientRepository;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class IngredientService {
+    private final IngredientRepository repository;
+    private final IngredientService service;
+
+    public List<Ingredient> getAllIngredients() throws SQLException {
+        return repository.findIngredient();
+    }
+}
