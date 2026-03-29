db7260c41f5633dd093e69e63c2fef721a311639
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-03-29 17:04:43 +0300

    feat: create IngredientRepository

---
+++ src/main/java/spring/restaurantmanagement/repository/IngredientRepository.java
@@ -0,0 +1,34 @@
+package spring.restaurantmanagement.repository;
+
+import lombok.AllArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import spring.restaurantmanagement.entity.CategoryEnum;
+import spring.restaurantmanagement.entity.Ingredient;
+
+import java.util.List;
+
+@Repository
+@AllArgsConstructor
+public class IngredientRepository {
+    JdbcTemplate jdbcTemplate;
+
+    public List<Ingredient> findIngredient() {
+        String sql= """
+                select id,
+                       name,
+                       category,
+                       price
+                """;
+
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            Ingredient ing = new Ingredient();
+            ing.setId(rs.getInt("id"));
+            ing.setName(rs.getString("name"));
+            ing.setCategory(CategoryEnum.valueOf(rs.getString("category")));
+            ing.setPrice(rs.getDouble("price"));
+
+            return ing;
+        });
+    }
+}
