73e7b97d0c4925ebdd422bcad2561513af4d7bf6
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-03-29 19:26:11 +0300

    feat: add GET /ingredients/{id} in controller

--- src/main/java/spring/restaurantmanagement/controller/IngredientController.java
+++ src/main/java/spring/restaurantmanagement/controller/IngredientController.java
@@ -1,12 +1,17 @@
 package spring.restaurantmanagement.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import spring.restaurantmanagement.entity.Ingredient;
+import spring.restaurantmanagement.exception.IngredientNotFoundException;
 import spring.restaurantmanagement.service.IngredientService;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,5 +22,16 @@ public class IngredientController {
     @GetMapping("/ingredients")
     public List<Ingredient> getIngredients() throws SQLException {
         return service.getAllIngredients();
+    }
+
+    @GetMapping("/ingredients/{id}")
+    public ResponseEntity<?> getIngredientById(@PathVariable int id){
+        try{
+            return ResponseEntity.ok(service.getIngredientById(id));
+        } catch (IngredientNotFoundException e){
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body(e.getMessage());
+        }
     }
 }
