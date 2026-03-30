aa7883af7226c012bd7449d73474032c2b7ea53e
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-03-30 21:19:57 +0300

    feat: add updateDishIngredient in service using transactional to commit update

--- src/main/java/spring/restaurantmanagement/service/DishService.java
+++ src/main/java/spring/restaurantmanagement/service/DishService.java
@@ -2,7 +2,10 @@ package spring.restaurantmanagement.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import spring.restaurantmanagement.entity.Dish;
+import spring.restaurantmanagement.entity.Ingredient;
+import spring.restaurantmanagement.exception.DIshNotFoundException;
 import spring.restaurantmanagement.repository.DishRepository;
 
 import java.util.List;
@@ -14,5 +17,16 @@ public class DishService {
 
     public List<Dish> getAllDishes(){
         return repository.findDish();
+    }
+
+    @Transactional
+    public void updateDishIngredients(int dishId, List<Ingredient> ingredients){
+        if(!repository.dishExists(dishId)){
+            throw new DIshNotFoundException(dishId);
+        }
+        repository.detachIngredients(dishId);
+        if(!ingredients.isEmpty()){
+            repository.attachIngredients(dishId, ingredients);
+        }
     }
 }
