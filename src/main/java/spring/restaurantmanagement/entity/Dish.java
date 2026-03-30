9216c7576184b0cbcc800377c707798e27733c98
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-03-30 18:49:48 +0300

    feat: create Dish class

---
+++ src/main/java/spring/restaurantmanagement/entity/Dish.java
@@ -0,0 +1,18 @@
+package spring.restaurantmanagement.entity;
+
+import lombok.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+public class Dish {
+    private int id;
+    private String name;
+    private Double sellingPrice;
+    private List<Ingredient> ingredientList;
+}
