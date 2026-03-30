81f8df8799bab7a992897ad73aeb744d8afd99b6
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-03-30 18:36:00 +0300

    feat: add GET /ingredients/{id}/stock?at={temporal}&unit={unit} in IngredientController

--- src/main/java/spring/restaurantmanagement/controller/IngredientController.java
+++ src/main/java/spring/restaurantmanagement/controller/IngredientController.java
@@ -5,13 +5,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import spring.restaurantmanagement.entity.Ingredient;
+import spring.restaurantmanagement.entity.UnitTypeEnum;
 import spring.restaurantmanagement.exception.IngredientNotFoundException;
 import spring.restaurantmanagement.service.IngredientService;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
+import java.time.Instant;
 import java.util.List;
 
 @RestController
@@ -33,5 +35,10 @@ public class IngredientController {
                     .status(HttpStatus.NOT_FOUND)
                     .body(e.getMessage());
         }
+    }
+
+    @GetMapping("/ingredients/{id}/stock")
+    public ResponseEntity<?> getStockValueAt(@PathVariable int id, @RequestParam (name = "at")Instant at, @RequestParam UnitTypeEnum unit) throws IngredientNotFoundException {
+        return ResponseEntity.ok(service.getStockValueAt(id, at, unit));
     }
 }
