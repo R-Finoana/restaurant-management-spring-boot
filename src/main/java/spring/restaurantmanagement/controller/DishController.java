c5c8051034c6375942c0b5caba690e5b7dbabe40
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-03-30 21:20:25 +0300

    feat: add PUT /dishes/{id}/ingredients in DishController

--- src/main/java/spring/restaurantmanagement/controller/DishController.java
+++ src/main/java/spring/restaurantmanagement/controller/DishController.java
@@ -2,9 +2,10 @@ package spring.restaurantmanagement.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import spring.restaurantmanagement.entity.Dish;
+import spring.restaurantmanagement.entity.Ingredient;
+import spring.restaurantmanagement.exception.DIshNotFoundException;
 import spring.restaurantmanagement.service.DishService;
 
 import java.util.List;
@@ -17,5 +18,11 @@ public class DishController {
     @GetMapping("/dishes")
     public ResponseEntity<List<Dish>> getDishes(){
         return ResponseEntity.ok(service.getAllDishes());
+    }
+
+    @PutMapping("/dishes/{id}/ingredients")
+    public ResponseEntity<?> updateDishIngredients(@PathVariable int id, @RequestBody List<Ingredient> ingredients) throws DIshNotFoundException {
+        service.updateDishIngredients(id, ingredients);
+        return ResponseEntity.ok().build();
     }
 }
