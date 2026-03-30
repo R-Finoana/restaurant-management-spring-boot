f52d51548a82b5675ea75b3dcd1fdaffdd649bf8
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-03-30 15:01:00 +0300

    feat: add getStockValueAt method in IngredientService

--- src/main/java/spring/restaurantmanagement/service/IngredientService.java
+++ src/main/java/spring/restaurantmanagement/service/IngredientService.java
@@ -3,10 +3,13 @@ package spring.restaurantmanagement.service;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import spring.restaurantmanagement.entity.Ingredient;
+import spring.restaurantmanagement.entity.StockValue;
+import spring.restaurantmanagement.entity.UnitTypeEnum;
 import spring.restaurantmanagement.exception.IngredientNotFoundException;
 import spring.restaurantmanagement.repository.IngredientRepository;
 
 import java.sql.SQLException;
+import java.time.Instant;
 import java.util.List;
 
 @Service
@@ -20,5 +23,9 @@ public class IngredientService {
 
     public Ingredient getIngredientById(int id) throws IngredientNotFoundException {
         return repository.findIngredientById(id);
+    }
+
+    public StockValue getStockValueAt(int id, Instant t, UnitTypeEnum unit) throws IngredientNotFoundException {
+        return repository.getStockValueAt(id, t, unit);
     }
 }
