a3078c73fceb6c353a710c7abd306a6536873041
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-03-30 23:41:08 +0300

    fix: resolve duplicate dishes in GET /dishes by using separate queries

--- src/main/java/spring/restaurantmanagement/repository/DishRepository.java
+++ src/main/java/spring/restaurantmanagement/repository/DishRepository.java
@@ -19,39 +19,35 @@ public class DishRepository {
     JdbcTemplate jdbcTemplate;
 
     public List<Dish> findDish(){
-        String sql = """
-                select d.id AS dish_id,
-                       d.name AS dish_name,
-                       d.selling_price AS dish_price,
-                       i.id AS ingredient_id,
-                       i.name AS ingredient_name,
-                       i.category AS ingredient_category,
-                       i.price AS ingredient_price
-                from dish d
-                         join dish_ingredient di
-                              on di.id_dish=d.id
-                         join ingredient i
-                              on di.id_ingredient=i.id;
+        String dishSql = "SELECT id, name, selling_price FROM dish";
+        String ingredientSql = """
+            select i.id, i.name, i.category, i.price
+            from ingredient i
+            join dish_ingredient di on di.id_ingredient = i.id
+            where di.id_dish = ?
             """;
 
-        return jdbcTemplate.query(sql, (rs, rowNum) -> {
-            List<Ingredient> ingredientList = new ArrayList<>();
-            Ingredient ing = new Ingredient();
-            ing.setId(rs.getInt("ingredient_id"));
-            ing.setName(rs.getString("ingredient_name"));
-            ing.setCategory(CategoryEnum.valueOf(rs.getString("ingredient_category")));
-            ing.setPrice(rs.getDouble("ingredient_price"));
-
-            ingredientList.add(ing);
-
+        List<Dish> dishes = jdbcTemplate.query(dishSql, (rs, rowNum) -> {
             Dish dish = new Dish();
-            dish.setId(rs.getInt("dish_id"));
-            dish.setName(rs.getString("dish_name"));
-            dish.setSellingPrice(rs.getDouble("dish_price"));
-            dish.setIngredientList(ingredientList);
-
+            dish.setId(rs.getInt("id"));
+            dish.setName(rs.getString("name"));
+            dish.setSellingPrice(rs.getDouble("selling_price"));
             return dish;
         });
+
+        for (Dish d : dishes) {
+            List<Ingredient> ingredients = jdbcTemplate.query(ingredientSql, (rs, rowNum) -> {
+                Ingredient ing = new Ingredient();
+                ing.setId(rs.getInt("id"));
+                ing.setName(rs.getString("name"));
+                ing.setCategory(CategoryEnum.valueOf(rs.getString("category")));
+                ing.setPrice(rs.getDouble("price"));
+                return ing;
+            }, d.getId());
+            d.setIngredientList(ingredients);
+        }
+
+        return dishes;
     }
 
     public void detachIngredients(Integer dishId) {
