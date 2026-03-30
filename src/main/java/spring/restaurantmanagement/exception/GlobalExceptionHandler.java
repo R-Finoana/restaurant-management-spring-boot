66add030143e2e319d55cafdd0776ec1a41913d1
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-03-30 18:27:16 +0300

    refactor: create GlobalExceptionHandler to centralize exceptions

---
+++ src/main/java/spring/restaurantmanagement/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package spring.restaurantmanagement.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IngredientNotFoundException.class)
+    public ResponseEntity<String> handleIngredientNotFound(IngredientNotFoundException e){
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(e.getMessage());
+    }
+}
