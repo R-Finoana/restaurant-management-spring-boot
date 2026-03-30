f7634b1e68909a5627ee173a09b108cf91822162
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-03-30 15:00:31 +0300

    feat: add getStockValueAt method in IngredientRepository

--- src/main/java/spring/restaurantmanagement/repository/IngredientRepository.java
+++ src/main/java/spring/restaurantmanagement/repository/IngredientRepository.java
@@ -6,8 +6,11 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import spring.restaurantmanagement.entity.CategoryEnum;
 import spring.restaurantmanagement.entity.Ingredient;
+import spring.restaurantmanagement.entity.StockValue;
+import spring.restaurantmanagement.entity.UnitTypeEnum;
 import spring.restaurantmanagement.exception.IngredientNotFoundException;
 
+import java.time.Instant;
 import java.util.List;
 
 @Repository
@@ -54,6 +57,32 @@ public class IngredientRepository {
                 ing.setPrice(rs.getDouble("price"));
 
                 return ing;
+            }, id);
+        } catch (EmptyResultDataAccessException e){
+            throw new IngredientNotFoundException(id);
+        }
+    }
+
+    public StockValue getStockValueAt(int id, Instant t, UnitTypeEnum unit) throws IngredientNotFoundException {
+        String sql= """
+                select
+                    stock_movement.id_ingredient,
+                    ingredient.name,
+                    sum(CASE WHEN stock_movement.type='OUT' THEN (stock_movement.quantity*(-1)) ELSE stock_movement.quantity END) as actual_quantity,
+                    unit
+                from stock_movement join ingredient on stock_movement.id_ingredient=ingredient.id
+                where stock_movement.creation_datetime<= ?
+                  and stock_movement.id_ingredient= ?
+                group by (id_ingredient, stock_movement.unit, ingredient.name);
+                """;
+
+        try{
+            return jdbcTemplate.queryForObject(sql, (rs, rowNum) -> {
+            StockValue stock = new StockValue();
+            stock.setQuantity(rs.getDouble("actual_quantity"));
+            stock.setUnit(UnitTypeEnum.valueOf(rs.getString("unit")));
+
+            return stock;
             }, id);
         } catch (EmptyResultDataAccessException e){
             throw new IngredientNotFoundException(id);
