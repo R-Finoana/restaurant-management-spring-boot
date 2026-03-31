028a1e8e6fb7eab638421be391bee9f57a78edcd
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-03-31 15:42:33 +0300

    refactor: change jdbcTemplate by using DataSource in DishRepository

--- src/main/java/spring/restaurantmanagement/repository/DishRepository.java
+++ src/main/java/spring/restaurantmanagement/repository/DishRepository.java
@@ -1,14 +1,15 @@
 package spring.restaurantmanagement.repository;
 
 import lombok.AllArgsConstructor;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import spring.restaurantmanagement.entity.CategoryEnum;
 import spring.restaurantmanagement.entity.Dish;
 import spring.restaurantmanagement.entity.Ingredient;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,35 +17,27 @@ import java.util.List;
 @Repository
 @AllArgsConstructor
 public class DishRepository {
-    JdbcTemplate jdbcTemplate;
+    DataSource dataSource;
 
     public List<Dish> findDish(){
         String dishSql = "SELECT id, name, selling_price FROM dish";
-        String ingredientSql = """
-            select i.id, i.name, i.category, i.price
-            from ingredient i
-            join dish_ingredient di on di.id_ingredient = i.id
-            where di.id_dish = ?
-            """;
+        List<Dish> dishes = new ArrayList<>();
 
-        List<Dish> dishes = jdbcTemplate.query(dishSql, (rs, rowNum) -> {
-            Dish dish = new Dish();
-            dish.setId(rs.getInt("id"));
-            dish.setName(rs.getString("name"));
-            dish.setSellingPrice(rs.getDouble("selling_price"));
-            return dish;
-        });
-
-        for (Dish d : dishes) {
-            List<Ingredient> ingredients = jdbcTemplate.query(ingredientSql, (rs, rowNum) -> {
-                Ingredient ing = new Ingredient();
-                ing.setId(rs.getInt("id"));
-                ing.setName(rs.getString("name"));
-                ing.setCategory(CategoryEnum.valueOf(rs.getString("category")));
-                ing.setPrice(rs.getDouble("price"));
-                return ing;
-            }, d.getId());
-            d.setIngredientList(ingredients);
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement ps = conn.prepareStatement(dishSql);
+             ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                Dish d = new Dish();
+                d.setId(rs.getInt("id"));
+                d.setName(rs.getString("name"));
+                d.setSellingPrice(rs.getDouble("selling_price"));
+                dishes.add(d);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        for (Dish dish : dishes) {
+            dish.setIngredientList(findIngredientsByDishId(dish.getId()));
         }
 
         return dishes;
@@ -57,20 +50,40 @@ public class DishRepository {
             join dish_ingredient di on di.id_ingredient = i.id
             where di.id_dish = ?
             """;
-        return jdbcTemplate.query(sql, (rs, rowNum) -> {
-            Ingredient ing = new Ingredient();
-            ing.setId(rs.getInt("id"));
-            ing.setName(rs.getString("name"));
-            ing.setCategory(CategoryEnum.valueOf(rs.getString("category")));
-            ing.setPrice(rs.getDouble("price"));
-            return ing;
-        }, dishId);
+        List<Ingredient> ingredients = new ArrayList<>();
+
+        try(Connection conn = dataSource.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);){
+
+            ps.setInt(1, dishId);
+
+            try (ResultSet rs = ps.executeQuery();){
+                while (rs.next()) {
+                    Ingredient ing = new Ingredient();
+                    ing.setId(rs.getInt("id"));
+                    ing.setName(rs.getString("name"));
+                    ing.setCategory(CategoryEnum.valueOf(rs.getString("category")));
+                    ing.setPrice(rs.getDouble("price"));
+
+                    ingredients.add(ing);
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return ingredients;
     }
 
     public void detachIngredients(Integer dishId) {
         String sql="DELETE FROM dish_ingredient WHERE id_dish = ?";
 
-        jdbcTemplate.update(sql, dishId);
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, dishId);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void attachIngredients(Integer dishId, List<Ingredient> ingredients){
@@ -79,15 +92,29 @@ public class DishRepository {
                 VALUES (?, ?)
                 """;
 
-        jdbcTemplate.batchUpdate(sql, ingredients, ingredients.size(), (ps, ingredient) -> {
-            ps.setInt(1, dishId);
-            ps.setInt(2, ingredient.getId());
-        });
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            for (Ingredient ing : ingredients) {
+                ps.setInt(1, dishId);
+                ps.setInt(2, ing.getId());
+                ps.addBatch();
+            }
+            ps.executeBatch();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public boolean dishExists(int dishId){
         String sql ="SELECT id from dish WHERE id = ?";
-        Integer count = jdbcTemplate.queryForObject(sql, Integer.class, dishId);
-    return count != null && count > 0;
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, dishId);
+            try (ResultSet rs = ps.executeQuery()) {
+                return rs.next();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
